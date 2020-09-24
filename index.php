e1a2c8e2f7f200725802ee81d5329a2f940896f7
Bradley Sloan <bradsloan16@gmail.com>
2020-09-23 20:14:46 -0400

    Old portfolio transfer

---
+++ index.php
@@ -0,0 +1,340 @@
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Bradley Sloan's Portfolio</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<link rel="stylesheet" href="assets/css/main.css" />
+	</head>
+
+	<nav>
+		<ul>
+		  <li><a href="#about">About</a></li>
+		  <li><a href="#projects">Products/Projects</a></li>
+		  <li><a href="#design">UX/UI Design</a></li>
+		  <li><a href="#marketing">Marketing</a></li>
+		  <li><a href="#film">Film</a></li>
+		  <li><a href="images/Bradley_Sloan_Resume.pdf" download>Download My Resume (PDF)</a></li>
+		  <li><a href="#footer">Contact</a></li>
+		</ul>
+	</nav>
+
+	<body class="homepage">
+		<div id="page-wrapper">
+			<!-- Header -->
+				<div id="header">
+					<!-- Inner -->
+						<div class="inner">
+							<header>
+								<h1>Bradley Sloan</h1>
+								<br>
+								<p>Product Manager & Software Engineer</p>
+							</header>
+							<footer>
+								<a href="#about" class="button circled scrolly">Go!</a>
+							</footer>
+						</div>
+				</div>
+
+				<div class="single-page" id="about">
+					<section id="banner">
+						<header>
+							<h2>About</h2>
+						</header>
+						<div class="about">
+							<p style="text-align: center;">
+								 I'm a full stack engineer with a background in UX/UI design and digital marketing.
+								 <br>
+								 This multifaceted expertise allows me to successfully manage products, engineers, designers and businesses.
+							</p>
+							<h3>EDUCATION</h3>
+							<ul>
+								<li><strong>University of Michigan</strong> - School of Information</li>
+								<li><strong>Dev Bootcamp NYC</strong> - Full Stack Developer Certificate</li>
+							</ul>
+							<h3>PROGRAMMING SKILLS</h3>
+							<ul>
+								<li>Ruby on Rails</li>
+								<li>HTML5 & CSS</li>
+								<li>SQL & ActiveRecord</li>
+								<li>Object Oriented Javascript
+								(AJAX, JQuery, React)</li>
+								<li>Bootstrap & AngularJS</li>
+								<li>Git & Source Control</li>
+								<li>Azure DevOps/VSTS & Balena Cloud</li>
+								<li>QA/Testing & Rspec</li>
+								<li>Heroku & Agile Development</li>
+								<li>Sublime Text, Atom & Coda</li>
+							</ul>
+							<h3>DESIGN SKILLS/OTHER</h3>
+							<ul>
+								<li>Adobe XD, Sketch & InVision</li>
+								<li>Adobe Photoshop, Adobe Illustrator & Adobe InDesign</li>
+								<li>Alexa for Business & AWS</li>
+								<li>Final Cut Pro</li>
+							</ul>
+						</div>
+					</section>
+				</div>
+
+				<div class="single-page" id="projects">
+					<!-- Banner -->
+					<section id="banner">
+						<header>
+							<h2>Products/Projects</h2>
+							<p>
+								Product/Project Manager & Web/App Development Experience
+							</p>
+						</header>
+					</section>
+
+				<!-- Carousel -->
+					<section class="carousel">
+						<div class="reel">
+							<article>
+								<a href="https://www.smartaisle.io/" target="_blank" class="image featured"><img src="images/SmartAisleLogo.jpg" alt="" /></a>
+								<header>
+									<h3><a href="https://www.smartaisle.io/" target="_blank">SmartAisle</a></h3>
+								</header>
+								<p>SmartAisle is the World's first voice-powered retail shopping assistant. It is an innovative, new technology platform designed specifically to revolutionize the customer experience in brick & mortar retail stores. Through a combination of an intuitive Voice interface, connected LED lights affixed to the shelf and a powerful artificial intelligence working behind the scenes, it provides expert product recommendations, education and support to shoppers at the all-important point of decision. As Software Product Manager, I am responsible for developing and maintaining SmartAisle's applications, backend systems and APIs. Throughout our development process, I manage a team of engineers, overseeing our build, QA and release phases.
+								<h4><a href="https://www.smartaisle.io/" target="_blank">Site Link</a></h4>
+							</article>
+							<article>
+								<a href="https://hurwitz-and-gantz.herokuapp.com/" target="_blank" class="image featured"><img src="images/hkwg.jpg" alt="" /></a>
+								<header>
+									<h3><a href="https://hurwitz-and-gantz.herokuapp.com/" target="_blank">Hurwitz & Gantz</a></h3>
+								</header>
+								<p>Hurwitz & Gantz, a Michigan based law firm, hired me to remake their website. For this project, I utilized a React front-end for a quick and clean interface. I also added an email feature so that each lawyer may be contacted directly from the website.
+								<h4><a href="https://github.com/bsloan3/HurwitzAndGantz" target="_blank">Source Code</a></h4>
+								<h4><a href="https://hurwitz-and-gantz.herokuapp.com/" target="_blank">Site Link</a></h4>
+							</article>
+							<article>
+								<a href="javascript:void(0);" class="image featured"><img src="images/mediamine.jpg" alt="" /></a>
+								<header>
+									<h3><a href="javascript:void(0);">MediaMine</a></h3>
+								</header>
+								<p>A personalized one-stop-shop for media consumption. Podcasts, Television, Movies, Sports, News and Music all compiled onto one dashboard with a calendar to track media events.
+								</p>
+								<h4><a href="https://github.com/bsloan3/MediaMine/" target="_blank">Rails Back-End</a></h4>
+								<h4><a href="https://github.com/bsloan3/MediaMine-React-FrontEnd/" target="_blank">React Front-End</a></h4>
+							</article>
+							<article>
+								<a href="https://movie-match-sloan.herokuapp.com/" target="_blank" class="image featured"><img src="images/moviematch.jpg" alt="" /></a>
+								<header>
+									<h3><a href="https://movie-match-sloan.herokuapp.com/" target="_blank">MovieMatch</a></h3>
+								</header>
+								<p>Users can get recommendations, review/score movies for their profile and see their friends reviews and recommendations. Utilizes an algorithm to recommend movies (pulls data from TMDB API).</p>
+								<h4><a href="https://github.com/bsloan3/movie-match" target="_blank">Source Code</a></h4>
+								<h4><a href="https://movie-match-sloan.herokuapp.com" target="_blank">Site Link</a></h4>
+							</article>
+						</div>
+					</section>
+				</div>
+
+			<div class="single-page" id="design">
+				<!-- Banner -->
+					<section id="banner">
+						<header>
+							<h2>User Experience Design</h2>
+							<p>
+								Wireframes and Prototypes
+							</p>
+						</header>
+					</section>
+
+				<!-- Carousel -->
+					<section class="carousel">
+						<div class="reel">
+							<article>
+								<a href="https://www.smartaisle.io/" target="_blank" class="image featured"><img src="images/SmartAisleWebsite.jpg" alt="" /></a>
+								<header>
+									<h3><a href="https://www.smartaisle.io/" target="_blank">SmartAisle Website</a></h3>
+								</header>
+								<p>SmartAisle's Website for product information, press mentions and contact information</p>
+							</article>
+							<article>
+								<a href="https://invis.io/MRAX6NM95/" target="_blank" class="image featured"><img src="images/LTPdesktop.jpg" alt="" /></a>
+								<header>
+									<h3><a href="https://invis.io/MRAX6NM95/" target="_blank">Website for LT&P</a></h3>
+								</header>
+								<p>Parking Permit Website for University of Michigan's Logistics, Transportation & Parking Division</p>
+							</article>
+							<article>
+								<a href="https://invis.io/CJB47TREN/" target="_blank" class="image featured"><img src="images/LTPmobile.jpg" alt="" /></a>
+								<header>
+									<h3><a href="https://invis.io/CJB47TREN/" target="_blank">Mobile Site for LT&P</a></h3>
+								</header>
+								<p>Parking Permit Website for University of Michigan's Logistics, Transportation & Parking Division (Optimized for Mobile)</p>
+							</article>
+							<article>
+								<a href="https://invis.io/EHCQ77CM7#/245275541_Home/" target="_blank" class="image featured"><img src="images/SJC.jpg" alt="" /></a>
+								<header>
+									<h3><a href="https://invis.io/EHCQ77CM7#/245275541_Home/" target="_blank">Screen Junkies Connect</a></h3>
+								</header>
+								<p>Mobile Application Connecting Members of the Screen Junkies Community</p>
+							</article>
+							<article>
+								<a class="image featured"><img src="images/mitech.jpg" alt="" /></a>
+								<header>
+									<h3>MITechNews.com Mobile App</a></h3>
+								</header>
+								<p>Mobile Application for MITechNews.com (Can Function for any News Site Utilizing a Wordpress API). Currently in the client's hands and is in production...</p>
+							</article>
+							<article>
+								<a class="image featured"><img src="images/CVlogo.jpg" alt="" /></a>
+								<header>
+									<h3>ComicsVerse Website</a></h3>
+								</header>
+								<p>I am a co-designer/developer of the ComicsVerse website as well as some of their pop-culture related content.</p>
+								<p><a href="https://comicsverse.com/author/fnsupercritic/" target="_blank">ComicsVerse Profile</a></p>
+							</article>
+						</div>
+					</section>
+				</div>
+
+				<div class="single-page" id="marketing">
+					<section id="banner">
+						<header>
+							<h2>Marketing</h2>
+							<p>
+								Outside of my work as a Software Product Manager at The Mars Agency, I work on many marketing campaigns, lending my digital expertise to the agency's multimillion dollar clients such as Lowe's, Walmart, Budweiser and T-Mobile.
+							</p>
+						</header>
+					</section>
+
+				<!-- Carousel -->
+					<section class="carousel">
+						<div class="reel">
+							<article>
+								<a class="image featured"><img src="images/SmartAisle_SellSheet_2019.jpg" alt="" /></a>
+								<header>
+									<h3><a href="images/SmartAisle_SellSheet_2019.pdf" download>SmartAisle Sell Sheet (PDF)</a></h3>
+								</header>
+								<p>A 2-page sell sheet for SmartAisle. This page details the what, how and whys of our platform to prospective clients.</p>
+							</article>
+							<article>
+								<a class="image featured"><img src="images/shelfie.jpg" alt="" /></a>
+								<header>
+									<h3>The Shelfie</a></h3>
+								</header>
+								<p>An in store shopper marketing promotion. Shoppers were to take a selfie with products in stores. They would then post it to their social media accounts with #(brandname)Shelfie for a chance to win a prize.</p>
+							</article>
+							<article>
+								<a class="image featured"><img src="images/chodip.jpg" alt="" /></a>
+								<header>
+									<h3>Chobani</a></h3>
+								</header>
+								<p>Chobani Mezé Dips was one of the new product launches to which the intern team was assigned. We presented our ideas to the client on how to best promote their new product to consumers.</p>
+							</article>
+							<article>
+								<a class="image featured"><img src="images/walmartfm.jpg" alt="" /></a>
+								<header>
+									<h3>Walmart Family Mobile</a></h3>
+								</header>
+								<p>T-Mobile's Walmart Family Mobile was one of the brands to which the intern team was assigned. We presented our ideas to the client on how to best promote their cellphone plans to convince users to switch to Walmart Family Mobile.</p>
+							</article>
+							<article>
+								<a class="image featured"><img src="images/Sobieski.jpg" alt="" /></a>
+								<header>
+									<h3>Sobieski Vodka</a></h3>
+								</header>
+								<p>During The Mars Agency's Sobieski microsite development, I acted as a user experience tester prior to launch. I also contributed ideas to their social media campaign during this time.</p>
+							</article>
+						</div>
+					</section>
+				</div>
+
+			<div class="single-page" id="film">
+			<!-- Banner -->
+				<section id="banner">
+					<header>
+						<h2>Film</h2>
+						<p>
+							Writer, Director, Producer, Editor
+						</p>
+					</header>
+				</section>
+
+			<!-- Carousel -->
+				<section class="carousel">
+					<div class="reel">
+						<article>
+							<a href="https://www.youtube.com/watch?v=hxgL6yceYAg" target="_blank" class="image featured"><img src="images/MIA.jpg" alt="" /></a>
+							<header>
+								<h3><a href="https://www.youtube.com/watch?v=hxgL6yceYAg" target="_blank">Morning In America (The Struggle To Prioritize) </a></h3>
+							</header>
+							<p>It's an average Saturday for two college students. Both have the same exam on Monday. One decides to hit the books hard. The other decides to party with friends. Watch as they each second guess how they have chosen to spend their day.</p>
+						</article>
+						<article>
+							<a href="https://www.youtube.com/watch?v=GPi8dSXBbZU" target="_blank" class="image featured"><img src="images/drunkcomicsspiderman.jpg" alt="" /></a>
+							<header>
+								<h3><a href="https://www.youtube.com/watch?v=GPi8dSXBbZU" target="_blank">Drunk Comic Book Origins: Spiderman</a></h3>
+							</header>
+							<p>This ComicsVerse original series features everyday comic book fans getting drunk and attempting to retell the stories of their favorite heroes. Words start to slur and memories get hazy as alcohol levels rise.</p>
+						</article>
+						<article>
+							<a href="https://www.youtube.com/watch?v=cRe-xKb3UPg" target="_blank" class="image featured"><img src="images/sjint.jpg" alt="" /></a>
+							<header>
+								<h3><a href="https://www.youtube.com/watch?v=cRe-xKb3UPg" target="_blank">Screen Junkies Interview at Wizard World Philadelphia</a></h3>
+							</header>
+							<p>This is my interview of the creators of one of the most popular online series (Honest Trailers).</p>
+						</article>
+						<article>
+							<a href="https://www.youtube.com/watch?v=ZCrdNI2SS24" target="_blank" class="image featured"><img src="images/marsintern.jpg" alt="" /></a>
+							<header>
+								<h3><a href="https://www.youtube.com/watch?v=ZCrdNI2SS24" target="_blank">The Mars Agency Intern Video 2014</a></h3>
+							</header>
+							<p>What is it like to Intern for The Mars Agency? This how-to infomercial will help you figure it out! But what's an infomercial without a deep James Earl Jones-esque voice over to guide the way?</p>
+						</article>
+
+					</div>
+				</section>
+			</div>
+
+			<!-- Footer -->
+				<div id="footer">
+							<div class="12u">
+								<!-- Socials -->
+									<section class="contact">
+										<header>
+											<h3><a href="#about">BACK TO HOMEPAGE</a></h3>
+											<h3>Email: bradsloan16@gmail.com</h3>
+											<h3><a href="https://github.com/bsloan3" target="_blank">GitHub</a></h3>
+											<br>
+											<h3>Want to see more? Follow my socials.</h3>
+										</header>
+										<ul class="icons">
+											<li><a href="https://twitter.com/Bradley_Sloan" target="_blank" class="icon fa-twitter"><span class="label">Twitter</span></a></li>
+											<li><a href="https://www.facebook.com/brad.sloan.756" target="_blank" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+											<li><a href="https://www.instagram.com/bsloan3/" target="_blank" class="icon fa-instagram"><span class="label">Instagram</span></a></li>
+											<li><a href="https://www.linkedin.com/in/bradley-sloan-13229691/" target="_blank" class="icon fa-linkedin"><span class="label">Linkedin</span></a></li>
+										</ul>
+									</section>
+
+								<!-- Copyright -->
+									<div class="copyright">
+										<ul class="menu">
+											<li>&copy; Bradley Sloan. All rights reserved.</li>
+										</ul>
+									</div>
+
+							</div>
+
+						</div>
+					</div>
+				</div>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrolly.min.js"></script>
+			<script src="assets/js/jquery.onvisible.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
